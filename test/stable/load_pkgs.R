9b0820a5d148ef1c6a361a00bdcdb150caa98284
Dongdong Kong <kongdd@users.noreply.github.com>
2018-10-30 23:51:27 +1100

    Revert "fix nth_max"
    This reverts commit 825426c75a5e455187ba522ac13f5df936d03c9f.

--- test/stable/load_pkgs.R
+++ test/stable/load_pkgs.R
@@ -66,14 +66,8 @@ IGBPnames_005 <- c("water", "ENF", "EBF", "DNF", "DBF", "MF" , "CSH",
 #' nth_max(x)
 nth_max <- function(x, n = 2){
     len <- length(x)
-
-    if (sum(!is.na(x)) <= n){
-        min(x, na.rm = T)
-    } else {
-        sort(x, decreasing = T)[n]
-    }
-    # i   <- len-n+1
-    # sort(x, partial=i, na.last=T)[i]
+    i   <- len-n+1
+    sort(x,partial=i)[i]
 }
 
 fix_null <- function(x, default = NA){
