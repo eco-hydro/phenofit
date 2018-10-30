d7a1e4fe563a88d3654d6462a4e3ce0531c442a7
Dongdong <kongdd.sysu@gmail.com>
2018-10-31 01:22:49 +1100

    merge master to gee_Whittaker

--- R/doubleLogistics_fitting.R
+++ R/doubleLogistics_fitting.R
@@ -42,12 +42,12 @@ FitDL.Zhang <- function(y, t = index(y), tout = t, optimFUN = I_optimx,
     e <- Init_param(y, t, w)
 
     FUN    <- "doubleLog.zhang"
-    prior  <- rbind(
+    prior  <- with(e, rbind(
         c(doy.mx   , mn, mx, doy[1]   , k  , doy[2]   , k  ),
         c(doy.mx   , mn, mx, doy[1]+t1, k*2, doy[2]-t1, k*2),
-        c(doy.mx   , mn, mx, doy[1]-t1, k/2, doy[2]+t2, k/2))
+        c(doy.mx   , mn, mx, doy[1]-t1, k/2, doy[2]+t2, k/2)))
 
-    param_lims <- lims[c('t0', 'mn', 'mx', 'sos', 'r', 'eos', 'r')]
+    param_lims <- e$lims[c('t0', 'mn', 'mx', 'sos', 'r', 'eos', 'r')]
     # param_lims$r[2] %<>% multiply_by(2)
     lower  <- sapply(param_lims, `[`, 1)
     upper  <- sapply(param_lims, `[`, 2)
@@ -66,14 +66,14 @@ FitAG <- function(y, t = index(y), tout = t, FUN, optimFUN = I_optimx,
     # print(ls.str(envir = e))
 
     FUN <- "doubleAG"
-    prior <- rbind(
+    prior <- with(e, rbind(
         c(doy.mx, mn, mx, 1/half    , 2, 1/half, 2),
         # c(doy.mx, mn, mx, 0.2*half, 1  , 0.2*half, 1),
         # c(doy.mx, mn, mx, 0.5*half, 1.5, 0.5*half, 1.5),
-        c(doy.mx, mn, mx, 1/(0.8*half), 3, 1/(0.8*half), 3))
+        c(doy.mx, mn, mx, 1/(0.8*half), 3, 1/(0.8*half), 3)))
     # referenced by TIMESAT
-    lower  <- c(lims$t0[1], lims$mn[1], lims$mx[1], 1/(1.4*half), 2, 1/(1.4*half), 2)
-    upper  <- c(lims$t0[2], lims$mn[2], lims$mx[2], 1/(0.1*half), 6, 1/(0.1*half), 6)
+    lower  <- e$lims %$% c(t0[1], mn[1], mx[1], 1/(1.4*e$half), 2, 1/(1.4*e$half), 2)
+    upper  <- e$lims %$% c(t0[2], mn[2], mx[2], 1/(0.1*e$half), 6, 1/(0.1*e$half), 6)
 
     optim_pheno(prior, FUN, y, t, tout, optimFUN, method, w, lower = lower, upper = upper, ...)#quick return
 }
@@ -85,11 +85,11 @@ FitDL.Beck <- function(y, t = index(y), tout = t, optimFUN = I_optimx,
     e <- Init_param(y, t, w)
 
     FUN   <- "doubleLog.beck"
-    prior <- rbind(
+    prior <- with(e, rbind(
         c(mn, mx, doy[1]   , k  , doy[2]   , k  ),
-        c(mn, mx, doy[1]+t1, k*2, doy[2]-t2, k*2))
+        c(mn, mx, doy[1]+t1, k*2, doy[2]-t2, k*2)))
 
-    param_lims <- lims[c('mn', 'mx', 'sos', 'r', 'eos', 'r')]
+    param_lims <- e$lims[c('mn', 'mx', 'sos', 'r', 'eos', 'r')]
     lower  <- sapply(param_lims, `[`, 1)
     upper  <- sapply(param_lims, `[`, 2)
 
@@ -112,13 +112,13 @@ FitDL.Elmore <- function(y, t = index(y), tout = t, optimFUN = I_optimx,
 
     # doy_q  <- quantile(t, c(0.1, 0.25, 0.5, 0.75, 0.9), na.rm = TRUE)
     FUN   <- "doubleLog.elmore"
-    prior <- rbind(
+    prior <- with(e, rbind(
         c(mn, mx - mn, doy[1]+t1, k*2.5  , doy[2]-t2, k*2.5  , 0.002),
         c(mn, mx - mn, doy[1]   , k*1.25 , doy[2]   , k*1.25 , 0.002),
         c(mn, mx - mn, doy[1]   , k*0.5  , doy[2]   , k*0.5  , 0.05),
-        c(mn, mx - mn, doy[1]-t1, k*0.25 , doy[2]+t2, k*0.25, 0.1))
+        c(mn, mx - mn, doy[1]-t1, k*0.25 , doy[2]+t2, k*0.25, 0.1)))
     # xpred <- m1 + (m2 - m7*t)*((1/(1 + exp((m3l - t)/m4l))) - (1/(1 + exp((m5l - t)/m6l))))
-    param_lims <- lims[c('mn', 'mx', 'sos', 'r', 'eos', 'r')]
+    param_lims <- e$lims[c('mn', 'mx', 'sos', 'r', 'eos', 'r')]
     lower  <- c(sapply(param_lims, `[`, 1), 0  )
     upper  <- c(sapply(param_lims, `[`, 2), Inf)
 
@@ -150,22 +150,22 @@ FitDL.Gu <- function(y, t = index(y), tout = t, optimFUN = I_optimx,
     method = "nlminb", w, ...) {
     e <- Init_param(y, t, w)
 
-    a  <- ampl
+    a  <- e$ampl
     b1 <- 0.1
     b2 <- 0.1
     c1 <- 1
     c2 <- 1
 
     FUN <- "doubleLog.gu"
-    prior <- rbind(
+    prior <- with(e, rbind(
         c(mn, a, a, doy[1]-t1, k/2 , doy[2]+t2, k/2, 1  , 1),
         c(mn, a, a, doy[1]   , k   , doy[2]   , k  , 2  , 2),
         c(mn, a, a, doy[1]   , k*2 , doy[2]   , k*2, 3  , 3),
         c(mn, a, a, doy[1]+t1, k*2 , doy[2]-t2, k*2, 0.5, 0.5),
-        c(mn, a, a, doy[1]+t1, k*3 , doy[2]-t2, k*3, 5  , 5))
+        c(mn, a, a, doy[1]+t1, k*3 , doy[2]-t2, k*3, 5  , 5)))
     # y0 + (a1/(1 + exp(-(t - t1)/b1))^c1) - (a2/(1 + exp(-(t - t2)/b2))^c2)
 
-    param_lims <- lims[c('mn', 'mx', 'mx', 'sos', 'r', 'eos', 'r')]
+    param_lims <- e$lims[c('mn', 'mx', 'mx', 'sos', 'r', 'eos', 'r')]
     lower  <- c(sapply(param_lims, `[`, 1), 0  , 0)
     upper  <- c(sapply(param_lims, `[`, 2), Inf, Inf)
 
@@ -180,30 +180,34 @@ FitDL.Klos <- function(y, t = index(y), tout = t, optimFUN = I_optimx,
 
     a1 <- 0
     a2 <- 0  #ok
-    b1 <- mn #ok
+    b1 <- e$mn #ok
     b2 <- 0  #ok
     c  <- 0.2 * max(y)  # ok
     ## very slightly smoothed spline to get reliable maximum
     # tmp <- smooth.spline(y, df = 0.5 * length(y))#, find error: 20161104, fix tomorrow
 
-    B1 <- 4/(doy.mx - doy[1])
-    B2 <- 3.2/(doy[2] - doy.mx)
-    m1 <- doy[1] + 0.5 * (doy.mx - doy[1])
-    m2 <- doy.mx + 0.5 * (doy[2] - doy.mx)
-    m1.bis <- doy[1]
-    m2.bis <- doy[2]
-    q1 <- 0.5  # ok
-    q2 <- 0.5  # ok
-    v1 <- 2    # ok
-    v2 <- 2    # ok
+    prior <- with(e, {
+        B1 <- 4/(doy.mx - doy[1])
+        B2 <- 3.2/(doy[2] - doy.mx)
+        m1 <- doy[1] + 0.5 * (doy.mx - doy[1])
+        m2 <- doy.mx + 0.5 * (doy[2] - doy.mx)
+        m1.bis <- doy[1]
+        m2.bis <- doy[2]
+        q1 <- 0.5  # ok
+        q2 <- 0.5  # ok
+        v1 <- 2    # ok
+        v2 <- 2    # ok    
 
-    FUN <- "doubleLog.klos"
-    prior <- rbind(
+        rbind(
         c(a1, a2, b1, b2, c, B1, B2, m1, m2, q1, q2, v1, v2),
         c(a1, a2, b1, 0.01, 0, B1, B2, m1, m2.bis, q1, 1, v1, 4),
         c(a1, a2, b1, b2, c, B1, B2, m1.bis, m2, q1, q2, v1, v2),
         c(a1, a2, b1, b2, c, B1, B2, m1, m2.bis, q1, q2, v1, v2),
         c(a1, a2, b1, b2, c, B1, B2, m1.bis, m2, q1, q2, v1, v2))
+    })
+    
+    
+    FUN <- "doubleLog.klos"
     optim_pheno(prior, FUN, y, t, tout, optimFUN, method, w, ...)#quickly return
 }
 
@@ -249,7 +253,7 @@ Init_param <- function(y, t, w){
         sos = c(min(t)         , doy.mx + deltaT),
         eos = c(doy.mx - deltaT, max(t))
     )
-    list2env(listk( mx, mn, ampl, doy, doy.mx,
+    listk( mx, mn, ampl, doy, doy.mx,
         deltaT, deltaY, half, t1, t2,
-        k, lims), envir = parent.frame())
+        k, lims)
 }
--- R/phenofit.R
+++ R/phenofit.R
@@ -67,11 +67,11 @@ D2 <- function(fit, ...) UseMethod('D2', fit)
 curvature <- function(fit, ...) UseMethod('curvature', fit)
 
 #' @export
-print.phenofit <- function(fit){
-    FUN <- get(fit$fun, mode = 'function')
+print.phenofit <- function(x, ...){
+    FUN <- get(x$fun, mode = 'function')
     cat(sprintf(" formula:\t%s\n", attr(FUN, "formula") ))
     cat("pars:\n")
-    print(fit$par)
+    print(x$par)
 }
 
 #' @rdname derivative
@@ -158,20 +158,20 @@ curvature.phenofit <- function(fit, numDeriv = FALSE, smspline = FALSE, ...){
 #'
 #' @inheritParams grad.phenofit
 #' @export
-plot.phenofit <- function(fit){
-    name <- deparse(substitute(fit))
+plot.phenofit <- function(x, ...){
+    name <- deparse(substitute(x))
 
-    pred <- last(fit$fits)                 #zoo obj
-    t    <- fit$tout
+    pred <- last(x$fits)                 #zoo obj
+    t    <- x$tout
 
     pop    <- t[which.max(pred)]
-    derivs <- curvature(fit)
+    derivs <- curvature(x)
 
     # e <- environment(D1)
     # der1_diff <- c(NA, diff(pred))
     # microbenchmark::microbenchmark(
-    #     gradient <- numDeriv::grad(fit$fun,  t, par = fit$par),
-    #     der1     <- D2(fit),
+    #     gradient <- numDeriv::grad(x$fun,  t, par = x$par),
+    #     der1     <- D2(x),
     #     diff1 <- c(NA, NA, diff(diff(values)))
     # )
     # summary(der1 - gradient)
@@ -181,7 +181,7 @@ plot.phenofit <- function(fit){
 
     multi_p <- max(par()$mfrow) == 1
     op <- ifelse(multi_p, par(mfrow = c(2, 4), oma = c(0, 0, 1, 0)), par())
-    plot(fit$data$t, fit$data$y,
+    plot(x$data$t, x$data$y,
         type= "b", pch = 20, cex = 1.3, col = "grey",
         main = "curve fitting VI", xlab = "Index", ylab = "VI")
     lines(t, pred); grid()
@@ -204,10 +204,10 @@ plot.phenofit <- function(fit){
     # plot(diff(der1_diff), main = "diff2")
 
     # k <- derivs$k
-    PhenoTrs(fit, IsPlot = TRUE, trs = 0.2)
-    PhenoTrs(fit, IsPlot = TRUE, trs = 0.5)
-    metrics <- PhenoGu(fit, IsPlot = TRUE)
-    metrics <- PhenoKl(fit, IsPlot = TRUE)
+    PhenoTrs(x, IsPlot = TRUE, trs = 0.2)
+    PhenoTrs(x, IsPlot = TRUE, trs = 0.5)
+    metrics <- PhenoGu(x, IsPlot = TRUE)
+    metrics <- PhenoKl(x, IsPlot = TRUE)
 
     # show figure title
     op <- par(new = TRUE, mfrow = c(1, 1), oma = rep(0, 4), mar = rep(0, 4))
