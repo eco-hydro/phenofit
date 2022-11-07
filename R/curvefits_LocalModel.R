8434565fa5f05a1e0f15a1ca67bfa497fe2b00d4
Dongdong Kong <kongdd@users.noreply.github.com>
2022-11-07 13:04:40 +0800

    CRAN v0.3.7: fix MacOS manual compiled error

--- R/curvefits_LocalModel.R
+++ R/curvefits_LocalModel.R
@@ -11,8 +11,8 @@
 #' 
 #' \deqn{
 #' F(t)= \begin{cases}
-#' \alpha(t) f_{L}(t)+[1-\alpha(t)] f_{C}(t) & t_{L}<t<t_{C} \\ 
-#' \beta(t) f_{C}(t)+[1-\beta(t)] f_{R}(t) & t_{C}<t<t_{R}\end{cases}
+#' \alpha(t) f_{L}(t)+[1-\alpha(t)] f_{C}(t), t_{L}<t<t_{C} \\ 
+#' \beta(t) f_{C}(t)+[1-\beta(t)] f_{R}(t), t_{C}<t<t_{R}\end{cases}
 #' }
 #' 
 #' @inheritParams curvefits
